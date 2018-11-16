12b2ad13ab95c44e63cc544590045a2d21a700a2
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 20:46:08 +0000

    automatically backed up by learn

---
+++ mytest.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for (i = 0; i < musicians.length ; i++ ){
+    newArray.push( (musicians[i] + " " + instruments[i]));
+  }
+}
