27758eee7736910cbddcc924dc19d96b11553e0d
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 20:53:59 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -1,16 +1,9 @@
- const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-
-
-
-function theBeatlesPlay(musicians, instruments){
-  var newArray = [];
-  for (i = 0; i < musicians.length ; i++ ){
-    newArray.push( (musicians[i] + " plays " + instruments[i]));
-  }
- 
-  return newArray
+function iLoveTheBeatles(n) {
+  var newArray = []
+  do {
+    n++
+    newArray.push("I love the Beatles!")
+  } while (n < 15)
 }
 
-console.log(theBeatlesPlay(musicians, instruments))
+console.log(iLoveTheBeatles(15))
