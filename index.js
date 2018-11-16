0a0cdf0ee575977015bc15cfdac336bdd2ee6710
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 20:48:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
-  for (i = 0; i < musicians.length ; i++ ){
+  for (var i = 0; i < musicians.length ; i++ ){
     newArray.push( (musicians[i] + " plays " + instruments[i]));
   }
  
