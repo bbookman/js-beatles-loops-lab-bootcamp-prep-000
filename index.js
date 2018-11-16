937a96724cb17349bdb91cff00235eded5c2451a
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 20:47:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
   for (i = 0; i < musicians.length ; i++ ){
-    newArray.push( (musicians[i] + " " + instruments[i]));
+    newArray.push( (musicians[i] + " plays " + instruments[i]));
   }
+ 
+  return newArray
 }
