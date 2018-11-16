4d1962df0191d02ad622c446c822453674fd4468
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-16 20:47:34 +0000

    automatically backed up by learn

--- mytest.js
+++ mytest.js
@@ -7,7 +7,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
   for (i = 0; i < musicians.length ; i++ ){
-    newArray.push( (musicians[i] + " " + instruments[i]));
+    newArray.push( (musicians[i] + " plays " + instruments[i]));
   }
  
   return newArray
